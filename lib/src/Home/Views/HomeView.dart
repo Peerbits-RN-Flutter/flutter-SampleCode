b835145675da9a4faf1bde132dd60665a70fc991
Mustafa Lokhandwala <76999173+mustafaMtfa@users.noreply.github.com>
2021-04-08 15:54:50 +0530

    clean up code

--- lib/src/Home/Views/HomeView.dart
+++ lib/src/Home/Views/HomeView.dart
@@ -1,10 +1,4 @@
-import 'package:Common/common/ApiFactory/Modules/AuthenticationAPI.dart';
-import 'package:Common/common/Utils/Utils.dart';
-import 'package:Common/common/Widgets/PBButton.dart';
-import 'package:Common/common/Widgets/PBTextInput.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:Common/src/Home/Views/DetailsView.dart';
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
@@ -40,27 +34,6 @@ class _MyHomePageState extends State<MyHomePage> {
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
-            ),
-            PBButton(
-              onPress: () {
-                Get.to(() => DetailsView());
-              },
-              child: Text(
-                "Press",
-                style: Theme.of(context)
-                    .textTheme
-                    .headline6
-                    ?.apply(color: Colors.white),
-              ),
-              elevation: 10.0,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(18.0),
-              child: PBTextInput(
-                controller: TextEditingController(),
-                isSimpleField: false,
-                labelText: "Enter Name",
-              ),
             ),
           ],
         ),
